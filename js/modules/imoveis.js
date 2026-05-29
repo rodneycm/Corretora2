fe64757be37f8102fc8979f50fcb2dd6628b666b
Rodney <techesimples@gmail.com>
2026-05-29 02:30:31 -0300

    modulo de imoveis criado

---
+++ js/modules/imoveis.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+
+    SITE_NAME: "Stephanie Campos Consultoria Imobiliária",
+
+    SITE_URL: "https://rodneycm.github.io/Corretora2",
+
+    WHATSAPP: "5521989321485",
+
+    API_URL: "./data/imoveis.json",
+
+    ITEMS_PER_PAGE: 12,
+
+    FEATURED_LIMIT: 6,
+
+    IMAGE_FALLBACK:
+        "./assets/imoveis/default.jpg"
+
+};
+
+export default CONFIG;
