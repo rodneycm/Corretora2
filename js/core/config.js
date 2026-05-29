3bd71d6fd97377a9c6622e79d97a862d6b87e0bf
Rodney <techesimples@gmail.com>
2026-05-29 02:28:13 -0300

    jjjj

--- js/core/config.js
+++ js/core/config.js
@@ -1,18 +1,20 @@
 const CONFIG = {
 
-    SITE_NAME:
-    "Stephanie Campos Consultoria Imobiliária",
+    SITE_NAME: "Stephanie Campos Consultoria Imobiliária",
 
-    SITE_URL:
-    "https://rodneycm.github.io/Corretora2",
+    SITE_URL: "https://rodneycm.github.io/Corretora2",
 
-    WHATSAPP:
-    "5521989321485",
+    WHATSAPP: "5521989321485",
 
-    TELEFONE:
-    "(21) 98932-1485",
+    API_URL: "./data/imoveis.json",
 
-    API_IMOVEIS:
-    "/data/imoveis.json"
+    ITEMS_PER_PAGE: 12,
+
+    FEATURED_LIMIT: 6,
+
+    IMAGE_FALLBACK:
+        "./assets/imoveis/default.jpg"
 
 };
+
+export default CONFIG;
