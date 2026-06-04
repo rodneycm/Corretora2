929978f1fc2abc8842a18c74f501459fd8e013e7
Rodney <techesimples@gmail.com>
2026-06-04 00:30:13 -0300

    atualizando as extensões das img dentro do json

--- js/core/config.js
+++ js/core/config.js
@@ -1,19 +1,40 @@
 const CONFIG = {
 
-    SITE_NAME: "Stephanie Campos Consultoria Imobiliária",
+    SITE_NAME:
+        "Stephanie Campos Consultoria Imobiliária",
 
-    SITE_URL: "https://rodneycm.github.io/Corretora2",
+    SITE_URL:
+        "https://rodneycm.github.io/Corretora2",
 
-    WHATSAPP: "5521989321485",
+    WHATSAPP:
+        "5521989321485",
 
-    API_URL: "./data/imoveis.json",
+    API_URL:
+        "./data/imoveis.json",
 
-    ITEMS_PER_PAGE: 12,
+    ITEMS_PER_PAGE:
+        12,
 
-    FEATURED_LIMIT: 6,
+    FEATURED_LIMIT:
+        6,
 
     IMAGE_FALLBACK:
-        "./assets/imoveis/default.jpg"
+        "./assets/imoveis/default.webp",
+
+    CIDADE_PADRAO:
+        "Teresópolis",
+
+    ESTADO_PADRAO:
+        "RJ",
+
+    MOEDA:
+        "BRL",
+
+    MAX_RELATED:
+        3,
+
+    MAX_HOME_DESTAQUES:
+        6
 
 };
 
